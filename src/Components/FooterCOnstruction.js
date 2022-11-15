c87a1e68c8f16dbadff8a71119cfa2826d153f70
marcinzygan <soul@o2.pl>
2022-11-15 09:50:55 +0100

    added construction lading page and footer

---
+++ src/Components/FooterCOnstruction.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FooterCOnstruction = () => {
+  return (
+    <div className="construction__footer">
+      <div>
+        Designed and developed by :{" "}
+        <a href="https://marcin-zygan.com">Marcin Zygan</a>
+      </div>
+    </div>
+  );
+};
+
+export default FooterCOnstruction;
