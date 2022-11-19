65d58cc73c9fad222e3222bf3ea506651f6bd963
marcinzygan <soul@o2.pl>
2022-11-19 11:55:07 +0100

    added redux-toolkit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,19 +4,6 @@ import Navbar from "./Navbar";
 const Header = () => {
   return (
     <header>
-      {/* <div className="header__container">
-        <div className="header__logo">
-          <p>LOGO</p>
-        </div>
-        <nav className="nav">
-          <ul>
-            <li className="nav__link">home</li>
-            <li className="nav__link">who we are</li>
-            <li className="nav__link">our offer</li>
-            <li className="nav__link">contact</li>
-          </ul>
-        </nav>
-      </div> */}
       <Navbar />
     </header>
   );
