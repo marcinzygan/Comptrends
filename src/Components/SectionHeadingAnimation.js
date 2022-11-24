7edde4da1790d831124124c238cdc6f20d6520c9
marcinzygan <soul@o2.pl>
2022-11-24 18:41:11 +0100

    added sectionHeading animation

--- src/Components/SectionHeadingAnimation.js
+++ src/Components/SectionHeadingAnimation.js
@@ -1,13 +1,71 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 const SectionHeadingAnimation = (props) => {
+  const randomWidth = Math.ceil(Math.random(85) * 100);
+  console.log(randomWidth);
   return (
     <div className="section__heading-container">
       <div className="section__animation">
-        <div className="section__line-1"></div>
-        <div className="section__line-2"></div>
-        <div className="section__line-3"></div>
-        <div className="section__line-4"></div>
+        <motion.div
+          className="section__line-1"
+          initial={{ height: "5px", width: "65px" }}
+          animate={{ width: "85px" }}
+          transition={{
+            ease: "easeInOut",
+            duration: 0.5,
+            repeatDelay: 5,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+        ></motion.div>
+        <motion.div
+          initial={{ height: "5px", width: "48px" }}
+          animate={{ width: "55px" }}
+          transition={{
+            ease: "easeInOut",
+            duration: 0.5,
+            repeatDelay: 5,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+          className="section__line-2"
+        ></motion.div>
+        <motion.div
+          initial={{ height: "5px", width: "85px" }}
+          animate={{ width: "50px" }}
+          transition={{
+            ease: "easeInOut",
+            duration: 0.5,
+            repeatDelay: 5,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+          className="section__line-3"
+        ></motion.div>
+        <motion.div
+          className="section__line-4"
+          initial={{ height: "5px", width: "62px" }}
+          animate={{ width: "35px" }}
+          transition={{
+            ease: "easeInOut",
+            duration: 0.5,
+            repeatDelay: 5,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+        ></motion.div>
+        <motion.div
+          className="section__line-5"
+          initial={{ height: "5px", width: "22px" }}
+          animate={{ width: "70px" }}
+          transition={{
+            ease: "easeInOut",
+            duration: 0.5,
+            repeatDelay: 5,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+        ></motion.div>
       </div>
       <h2>{props.heading}</h2>
     </div>
